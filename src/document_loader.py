86171ed9c3aea03c7a45eccd8a8755f135e6fa3c
Dinesh <87664339+Dineshp2903@users.noreply.github.com>
2025-02-13 00:35:22 +0530

    Update document_loader.py

--- src/document_loader.py
+++ src/document_loader.py
@@ -2,6 +2,11 @@ from abc import ABC,abstractmethod
 from constant import PDF_FILE_PATH
 from langchain_community.document_loaders import PyPDFLoader
 import os
+from vectordb import VectorDB
+from functools import lru_cache
+from llmexecutor import Groq
+from prompt import ChatGroqPrompt
+
 
 
 class DocumentLoader(ABC):
@@ -11,9 +16,12 @@ class DocumentLoader(ABC):
 
 
 class PDFLoader(DocumentLoader):
+    
+    @lru_cache(maxsize=128)
     def load(self,source:str):
         try:
             # Load all the files in the pdf directory
+            print("Loading files")
 
             list_of_links = [os.path.join(PDF_FILE_PATH, file) for file in os.listdir(PDF_FILE_PATH) if os.path.isfile(os.path.join(PDF_FILE_PATH, file))]
 
@@ -22,10 +30,6 @@ class PDFLoader(DocumentLoader):
                 loader = PyPDFLoader(link)
                 pages.extend(loader.load_and_split())
             print("Loaded successfully")
+            return pages
         except Exception as e:
             return f"Error getting files: {e}"
-        
-
-if __name__ == "__main__":
-    loader = PDFLoader()
-    loader.load(PDF_FILE_PATH)
